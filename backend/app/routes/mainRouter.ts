432b3e6bc0280bdeaf4f62ff6dc449d501f4c66a
tonikutlesa <tkutlesa_ag@medable.com>
2023-01-26 20:34:23 +0100

    adding course and enrollment route and controller

--- backend/app/routes/mainRouter.ts
+++ backend/app/routes/mainRouter.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import studentRouter from "./studentRouter";
 import professorRouter from "./professorRouter";
+import enrollmentRouter from "./enrollmentRouter";
+import courseRouter from "./courseCouter";
 
 const router = Router();
 
 router.use("/students", studentRouter);
 router.use("/professors", professorRouter);
+router.use("/enrollments", enrollmentRouter);
+router.use("/courses", courseRouter);
 
 export default router;
